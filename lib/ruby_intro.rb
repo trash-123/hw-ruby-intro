52df7af5fa418a81c5bc03dd18d804cdd8e9339a
trash_123 <trash_123@hotmail.com>
2015-10-23 21:31:55 +0000

    First assigment finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,59 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  accum = 0
+  arr.each {|elem| accum += elem }
+  accum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  accum = 0
+  sorted_array = arr.sort
+  
+  accum = sorted_array.pop if !sorted_array.empty?
+  accum += sorted_array.pop if !sorted_array.empty?
+  
+  accum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  index = 1
+  arr.sort.each do |elem|
+    accum = elem
+    return true if arr[index..(arr.length-1)].any? { |elem2| accum + elem2 == n }
+    index += 1
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  (s =~ /\A[bcdfghjklmnpqrstvwxyz]/i) == 0
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+ !s.empty? and s.each_char.all? { |char| char == '1' or char == '0' } and (s.to_i(2) % 4 == 0)
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'Bad argument' if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
+
